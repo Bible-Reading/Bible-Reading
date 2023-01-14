22884b5af372b7bdc544c5d46466b04dba471f3f
Bible-Reading <122586032+Bible-Reading@users.noreply.github.com>
2023-01-14 10:23:45 -0800

    Save

--- assets/JS/get_json.js
+++ assets/JS/get_json.js
@@ -7,21 +7,21 @@ function get_json(language) {
     Language = language;
     let parsed_json;
 
-    loading_box_update("Start requesting from the server json file");
+    console.log("Start requesting from the server json file");
     fetch('https://raw.githubusercontent.com/Bible-Reading/json/main/' + language + '.json')
         .then(
             function (response) {
-                loading_box_update("Start parsing the returned Json file");
+                console.log("Start parsing the returned Json file");
                 return response.json();
             })
         .then(function (json) {
             console.log(json);
-            loading_box_update("The Json file is parsed successfully");
+            console.log("The Json file is parsed successfully");
             Bible = json;
         })
         .catch((error) => {
-            loading_box_update("The Json file is parsed DFEATED");
+            console.log("The Json file is parsed DFEATED");
             console.log('%c' + error, "font-weight:bold;");
         });
-    loading_box_update("variable: 'Bible' are The json");
+        console.log("variable: 'Bible' are The json");
 }

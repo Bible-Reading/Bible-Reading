88fd78d050bc2744ae0b1915b1e25001cf90afd4
Bible-Reading <122586032+Bible-Reading@users.noreply.github.com>
2023-01-14 10:29:04 -0800

    Save

--- assets/JS/analyze_json.js
+++ assets/JS/analyze_json.js
@@ -15,7 +15,7 @@ function analyze_json(book, chapter, verse) {
             }
         }
         Analyze_json = Revise;
-        loading_box_update("Since there are no valid chapters, the entire book will be returned directly.");
+        console.log("Since there are no valid chapters, the entire book will be returned directly.");
     } else if (verse == undefined || verse == null || verse === "") {
         Analyze_json = Bible[book].chapters[chapter];
         let Revise = "";
@@ -25,13 +25,13 @@ function analyze_json(book, chapter, verse) {
             Revise += "<br><br>";
         }
         Analyze_json = Revise;
-        loading_box_update("The entire chapter is returned because there is no valid verse.");
+        console.log("The entire chapter is returned because there is no valid verse.");
     } else {
         Analyze_json += "<div style='margin: auto; font-size: xx-large;'>";
         Analyze_json += Bible[book].chapters[chapter][verse];
         Analyze_json += "</div>";
-        loading_box_update("All information will be returned" + "chapter" + chapter + " ," + "verse" + verse + ".");
+        console.log("All information will be returned" + "chapter" + chapter + " ," + "verse" + verse + ".");
     }
-    loading_box_update("Successfully analyze Json file:");
+    console.log("Successfully analyze Json file:");
     return Analyze_json;
 }

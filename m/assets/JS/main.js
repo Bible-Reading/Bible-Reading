e255dc0250517bbbe040631e6dfe6fe919306bb0
Bible-Reading <122586032+Bible-Reading@users.noreply.github.com>
2023-01-16 14:49:36 -0800

    Update main.js

--- m/assets/JS/main.js
+++ m/assets/JS/main.js
@@ -21,31 +21,31 @@ function Language_check(SLA) {
     if (book_language != null && book_language != "") {
         back_tur = book_language;
     } else {
-        if (SLA = "ru") {
+        if (SLA == "ru") {
             back_tur = "ru_synodal";
             localStorage.setItem("book_language", "ru_synodal");
-        } else if (SLA = "ar") {
+        } else if (SLA == "ar") {
             back_tur = "ar_svd";
             localStorage.setItem("book_language", "ar_svd");
-        } else if (SLA = "de") {
+        } else if (SLA == "de") {
             back_tur = "en_bbe";
             localStorage.setItem("book_language", "en_bbe");
-        } else if (SLA = "en") {
+        } else if (SLA == "en") {
             back_tur = "en_bbe";
             localStorage.setItem("book_language", "en_bbe");
-        } else if (SLA = "es") {
+        } else if (SLA == "es") {
             back_tur = "es_rvr";
             localStorage.setItem("book_language", "es_rvr");
-        } else if (SLA = "fi") {
+        } else if (SLA == "fi") {
             back_tur = "fi_finnish";
             localStorage.setItem("book_language", "fi_finnish");
-        } else if (SLA = "it") {
+        } else if (SLA == "it") {
             back_tur = "en_bbe";
             localStorage.setItem("book_language", "en_bbe");
-        } else if (SLA = "zh") {
+        } else if (SLA == "zh") {
             back_tur = "zh_cuv";
             localStorage.setItem("book_language", "zh_cuv");
-        } else if (SLA = "kr") {
+        } else if (SLA == "kr") {
             back_tur = "ko_ko";
             localStorage.setItem("book_language", "ko_ko");
         }

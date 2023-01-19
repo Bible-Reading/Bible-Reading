f0a9d8a2f08e47c66ed58d4c38c916acf0b41460
Bible-Reading <122586032+Bible-Reading@users.noreply.github.com>
2023-01-18 19:31:31 -0800

    Save

---
+++ assets/JS/External_Analysis_Interface.js
@@ -0,0 +1,27 @@
+function External_Analysis_Interface() {
+    let Html_Head = document.getElementsByTagName("head")[0];
+
+    let Clarity_Dom = '<script type="text/javascript">' +
+        '(function (c, l, a, r, i, t, y) {' +
+        'c[a] = c[a] || function () {' +
+        '(c[a].q = c[a].q || []).push(arguments)' +
+        '}; ' +
+        't = l.createElement(r);' +
+        't.async = 1;' +
+        't.src = "https://www.clarity.ms/tag/" + i;' +
+        'y = l.getElementsByTagName(r)[0];' +
+        'y.parentNode.insertBefore(t, y);' +
+        '})(window, document, "clarity", "script", "feaq3yn0zx");' +
+        '</script >';
+
+    let Google_Analytics_Dom = '<script async src="https://www.googletagmanager.com/gtag/js?id=G-5PDW7BXHLV"></script>' +
+        '<script>' +
+        'window.dataLayer = window.dataLayer || [];' +
+        'function gtag(){dataLayer.push(arguments);}' +
+        "gtag('js', new Date());" +
+        "gtag('config', 'G-5PDW7BXHLV');";
+    '</script>';
+
+    Html_Head.append(Clarity_Dom);
+    Html_Head.append(Google_Analytics_Dom);
+}

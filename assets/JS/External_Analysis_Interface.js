2f43cbae14c37112a711b50186769ac3d4a686a1
Bible-Reading <122586032+Bible-Reading@users.noreply.github.com>
2023-01-18 20:22:01 -0800

    Save

--- assets/JS/External_Analysis_Interface.js
+++ assets/JS/External_Analysis_Interface.js
@@ -1,4 +1,4 @@
-function External_Analysis_Interface() {
+/*function External_Analysis_Interface() {
     let Html_Head = document.getElementsByTagName("head")[0];
 
     let Clarity_Dom = document.createElement("script");
@@ -39,4 +39,9 @@ function External_Analysis_Interface() {
     document.write(Google_Analytics_Dom);
     document.write(Google_Ad_Dom);
 }
-External_Analysis_Interface();
+External_Analysis_Interface();*/
+
+document.write('<script type="text/javascript"> (function(c, l, a, r, i, t, y) { c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) }; t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i; y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y); })(window, document, "clarity", "script", "feaq3yn0zx"); </script>');
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-5PDW7BXHLV"></script>');
+document.write("<script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-5PDW7BXHLV'); </script>");
+document.write('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8498621770647817" crossorigin="anonymous"></script>');
